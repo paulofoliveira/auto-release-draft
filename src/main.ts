f541cc51431eacd376c8a8dc3d44f8bfd8f87cae
paulofoliveira <paulo.fdo@hotmail.com>
2023-02-26 11:55:57 -0300

    Corrigido erro No files matching the pattern do eslint + atualização do Teste

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import * as core from '@actions/core'
 
 export async function run(): Promise<void> {
   try {
-
     core.setOutput('release-url', 'https://example.com')
-
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
